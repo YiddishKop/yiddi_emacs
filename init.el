3a3eea9fdcc4052dd2bd70e7c3fcaee62e4e8f20
yid@ubuntu16.4@home <yiddishkop@163.com>
2017-02-06 22:44:51 -0800

    another modify version all-in-init.el

--- init.el
+++ init.el
@@ -58,7 +58,33 @@
 					    ;; emacs regex
 					    ("8ms" "MicroSoft")
 					    ))
-
+;; [set]DIY indent automaticly without selected-region
+(defun indent-buffer ()
+  "indent the currently visited buffer"
+  (interactive)
+  (indent-region (point-min) (point-max)))
+(defun indent-region-or-buffer ()
+  "indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+	(progn
+	  (indent-region (region-beginning) (region-end))
+	  (message "indent selected region."))
+      (progn
+	(indent-buffer)
+	(message "indented buffer.")))))
+;; [set]hippie-expand
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+					 try-expand-dabbrev-all-buffers
+					 try-expand-dabbrev-from-kill
+					 try-complete-file-name
+					 try-complete-file-name-partially
+					 try-complete-lisp-symbol
+					 try-complete-lisp-symbol-partially
+					 try-expand-all-abbrevs
+					 try-expand-line
+					 try-expand-line))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; [kb]bind a hotkey to open my init.el file
@@ -80,6 +106,10 @@
 (global-set-key (kbd "C-h C-v") 'find-variable)
 ;; [kb] find-function-on-key
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
+;; [kb] DIY-auto-indent
+(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
+;; [kb] hippie-expand
+(global-set-key (kbd "s-/") 'hippie-expand)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; ---------------pkg settings------------------
